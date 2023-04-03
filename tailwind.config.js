eb16c5b6014515ff484558b1c4387033244ab405
Raj Thombare <therajthombare@gmail.com>
2023-04-04 03:52:20 +0530

    animate projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,29 @@ module.exports = {
         slab: ['var(--font-roboto-slab)'],
         poppins: ['var(--font-poppins)'],
       },
+      animation: {
+        slideUpCubiBezier: "slideUp 1s cubic-bezier(0.165, 0.84, 0.44, 1)",
+      },
+      animationDelay: {
+        0: "0s",
+        2: "0.2s",
+        4: "0.4s",
+        6: "0.6s",
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideUp: {
+          from: { transform: "translateY(100%)" },
+          to: { transform: "translateY(0)" },
+        },
+        bounce: {
+          from: { transform: "translateY(10px)" },
+          to: { transform: "translateY(0)" },
+        },
+      },
       colors: {
         'white-rgba': 'rgb(255 255 255 / 59%)'
       },
