ab8947e342e7239693d32224f41503ed34b1e434
Raj Thombare <therajthombare@gmail.com>
2023-04-04 23:47:22 +0530

    refactor rendering footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,103 +1,90 @@
 import Link from "next/link"
 
 const Footer = () => {
+
+  const footerLinks = [
+    {
+      title: "About",
+      link: "#about",
+    },
+    {
+      title: "Projects",
+      link: "/projects",
+    },
+    {
+      title: "Posts",
+      link: "/blogs",
+    },
+    {
+      title: "Connect",
+      link: "#contact",
+    },
+  ];
+
+  const socialLinks = [
+    {
+      title: "Github",
+      link: "https://github.com/Raj-Thombare/",
+    },
+    {
+      title: "LinkedIn",
+      link: "https://www.linkedin.com/in/rajthombare/",
+    },
+    {
+      title: "Leetcode",
+      link: "https://leetcode.com/Rakaa/",
+    },
+    {
+      title: "Twitter",
+      link: "https://twitter.com/saysrakaa/",
+    },
+    {
+      title: "Hashnode",
+      link: "https://rajthombare.hashnode.dev/",
+    },
+    {
+      title: "Instagram",
+      link: "https://instagram.com/im.rakaa/",
+    },
+  ];
+
   return (
     <section className="mb-3 px-10 pb-1 md:pb-0 border-t-2 pt-1 md:pt-[2px]">
       <div className="flex flex-col md:flex-row mb-6 md:mb-10">
         <div className="text-black mr-0 mb-2.5 md:mb-0 md:mr-[100px]">
           <p className="pt-4 md:pb-2 text-[20px] font-medium">Quick Links</p>
           <ul>
-            <li className="p-0 py-1">
-              <Link
-                href="#about"
-                className="font-normal text-black text-[16px]"
-              >
-                About
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="/projects"
-                className="font-normal text-black text-[16px]"
-              >
-                Projects
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="/blogs"
-                className="font-normal text-black text-[16px]"
-              >
-                Blogs
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="#contact"
-                className="font-normal text-black text-[16px]"
-              >
-                Contact
-              </Link>
-            </li>
+            {footerLinks.map((footer) => {
+              return (
+                <li key={footer.title} className="p-0 py-1">
+                  <Link
+                    href={footer.link}
+                    className="font-normal text-black text-[16px]"
+                  >
+                    {footer.title}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
+
         <div className="text-black">
           <p className="pt-4 md:pb-2 text-[20px] font-medium">Socials</p>
           <ul>
-            <li className="p-0 py-1">
-              <Link
-                href="https://github.com/Raj-Thombare/"
-                target="_blank"
-                className="font-normal text-black text-[16px]"
-              >
-                GitHub
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="https://www.linkedin.com/in/rajthombare/"
-                target="_blank"
-                className="font-normal text-black text-[16px]"
-              >
-                LinkedIn
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="https://leetcode.com/Rakaa/"
-                target="_blank"
-                className="font-normal text-black text-[16px]"
-              >
-                Leetcode
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="https://twitter.com/saysrakaa/"
-                target="_blank"
-                className="font-normal text-black text-[16px]"
-              >
-                Twitter
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="https://rajthombare.hashnode.dev/"
-                target="_blank"
-                className="font-normal text-black text-[16px]"
-              >
-                Hashnode
-              </Link>
-            </li>
-            <li className="p-0 py-1">
-              <Link
-                href="https://instagram.com/im.rakaa/"
-                target="_blank"
-                className="font-normal text-black text-[16px]"
-              >
-                Instagram
-              </Link>
-            </li>
+            {socialLinks.map((social) => {
+              return (
+                <li key={social.title} className="p-0 py-1">
+                  <Link
+                    href={social.link}
+                    target="_blank"
+                    className="font-normal text-black text-[16px]"
+                  >
+                    {social.title}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
@@ -109,10 +96,14 @@ const Footer = () => {
             href="https://rajthombare.vercel.app"
             target="_blank"
           >
-            Raj Thombare
+            • Raj Thombare
           </a>
         </p>
-        <p>| All Rights Reserved</p>
+        <p>
+          Built with
+          <span className="mx-1 text-[#8a2be2]">NextJs</span> and
+          <span className="mx-1 text-[#8a2be2]">Tailwind</span>
+        </p>
       </div>
     </section>
   );
