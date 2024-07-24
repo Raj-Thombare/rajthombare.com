25e32a24387d80682432a61c12f6a348facd13b1
Raj Thombare <therajthombare@gmail.com>
2024-07-24 14:41:36 +0530

    feat:add project page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,35 @@
-import { Post } from "@/components/shared/Posts";
-import { post } from "@/components/data/projects";
+import { getPageContent, getPageBySlug, notion } from "@/lib/notion";
+import { NotionRenderer } from "@notion-render/client";
+import { notFound } from "next/navigation";
 
-export default async function Page() {
-  return <div>Projects page</div>;
+//Plugins
+import hljsPlugin from "@notion-render/hljs-plugin";
+import bookmarkPlugin from "@notion-render/bookmark-plugin";
+import { Post } from "@/components/shared/Posts";
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const post = await getPageBySlug(params.slug);
+
+  if (!post) notFound();
+
+  const content = await getPageContent(post.id);
+
+  const notionRenderer = new NotionRenderer({
+    client: notion,
+  });
+
+  notionRenderer.use(hljsPlugin({}));
+  notionRenderer.use(bookmarkPlugin(undefined));
+  const html = await notionRenderer.render(...content);
+
+  return (
+    <Post
+      title={(post.properties.Name as any).title[0].plain_text}
+      projectUrl={(post.properties.ProjectUrl as any).url}
+      githubUrl={(post.properties.GithubUrl as any).url}
+      date={(post.properties.Date as any).date.start}
+      content={html}
+      slug={params.slug}
+    />
+  );
 }
--- lib/date.ts
+++ lib/date.ts
@@ -1,5 +1,6 @@
 export function formatDate(date: string) {
     return new Date(date).toLocaleDateString("en-US", {
+        day: "numeric",
         month: "short",
         year: "numeric",
     });
